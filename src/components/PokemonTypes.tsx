d3658d11e36e98463a7e97461f0a22d3966c6cc1
Alexander Suarez <alexander.suarez@itksquare.edu.mx>
2023-04-29 20:20:05 -0600

    Type Colors added

--- src/components/PokemonTypes.tsx
+++ src/components/PokemonTypes.tsx
@@ -1,15 +1,29 @@
 import React from 'react'
+import typeColors from "../logic/typeColors";
+
 
 type Props = {
     types : string[];
 }
 
-const PokemonTypes = ({types}: Props) => {
+const PokemonTypes = ({ types }: Props) => {
   return (
     <ul>
-    {types.map((value) => <li key={value}>{value}</li>)}
-  </ul>
-  )
-}
+      {types.map((value) => (
+        <li
+          key={value}
+          style={{
+            backgroundColor: typeColors[value],
+            borderRadius: "5px",
+            padding: "2px 5px",
+          }}
+        >
+          {value}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
 
 export default PokemonTypes
